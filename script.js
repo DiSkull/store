3358f5a1fb65b5f11f6763ee3872ecf9a3302705
D-Prog <65495552+DiSkull@users.noreply.github.com>
2022-12-21 09:04:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+let pop = document.querySelector(".modal");
+
+
+let popw1 = document.getElementById("modalw1");
+let popw2 = document.getElementById("modalw2");
+let popw3 = document.getElementById("modalw3");
+let popw4 = document.getElementById("modalw4");
+let popw5 = document.getElementById("modalw5");
+let popw6 = document.getElementById("modalw6");
+let popw7 = document.getElementById("modalw7");
+let popw8 = document.getElementById("modalw8");
+let popw9 = document.getElementById("modalw9");
+let popw10 = document.getElementById("modalw10");
+let popw11 = document.getElementById("modalw11");
+let popw12 = document.getElementById("modalw12");
+let popw13 = document.getElementById("modalw13");
+let popw14 = document.getElementById("modalw14");
+let popw15 = document.getElementById("modalw15");
+
+
+function showPop1() {
+	pop.style.visibility = 'visible'
+	popw1.style.visibility = "visible"
+}
+
+function showPop2() {
+	pop.style.visibility = 'visible'
+	popw2.style.visibility = "visible"
+}
+
+function showPop3() {
+	pop.style.visibility = 'visible'
+	popw3.style.visibility = "visible"
+}
+
+function showPop4() {
+	pop.style.visibility = 'visible'
+	popw4.style.visibility = "visible"
+}
+
+function showPop5() {
+	pop.style.visibility = 'visible'
+	popw5.style.visibility = "visible"
+}
+
+function showPop6() {
+	pop.style.visibility = 'visible'
+	popw6.style.visibility = "visible"
+}
+
+function showPop7() {
+	pop.style.visibility = 'visible'
+	popw7.style.visibility = "visible"
+}
+
+function showPop8() {
+	pop.style.visibility = 'visible'
+	popw8.style.visibility = "visible"
+}
+
+function showPop9() {
+	pop.style.visibility = 'visible'
+	popw9.style.visibility = "visible"
+}
+
+function showPop10() {
+	pop.style.visibility = 'visible'
+	popw10.style.visibility = "visible"
+}
+
+function showPop11() {
+	pop.style.visibility = 'visible'
+	popw11.style.visibility = "visible"
+}
+
+function showPop12() {
+	pop.style.visibility = 'visible'
+	popw12.style.visibility = "visible"
+}
+
+function showPop13() {
+	pop.style.visibility = 'visible'
+	popw13.style.visibility = "visible"
+}
+
+function showPop14() {
+	pop.style.visibility = 'visible'
+	popw14.style.visibility = "visible"
+}
+
+function showPop15() {
+	pop.style.visibility = 'visible'
+	popw15.style.visibility = "visible"
+}
+
+
+function hidePop() {
+	pop.style.visibility = 'hidden'
+	popw1.style.visibility = "hidden"
+	popw2.style.visibility = "hidden"
+	popw3.style.visibility = "hidden"
+	popw4.style.visibility = "hidden"
+	popw5.style.visibility = "hidden"
+	popw6.style.visibility = "hidden"
+	popw7.style.visibility = "hidden"
+	popw8.style.visibility = "hidden"
+	popw9.style.visibility = "hidden"
+	popw10.style.visibility = "hidden"
+	popw11.style.visibility = "hidden"
+	popw12.style.visibility = "hidden"
+	popw13.style.visibility = "hidden"
+	popw14.style.visibility = "hidden"
+	popw15.style.visibility = "hidden"
+}
